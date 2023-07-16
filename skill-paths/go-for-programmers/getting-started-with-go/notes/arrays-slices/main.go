f1f762007069d251cc7741d55cf7aa5360138763
Rebecca <beca.dance@gmail.com>
2023-07-15 22:11:39 -0700

    arrays lesson 1

--- skill-paths/go-for-programmers/getting-started-with-go/notes/arrays-slices/main.go
+++ skill-paths/go-for-programmers/getting-started-with-go/notes/arrays-slices/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func f(a [3]int)   { fmt.Println(a) }
+func fp(a *[3]int) { fmt.Println(a) }
+
 func main() {
-	fmt.Println("hello world")
+	var arr [5]int
+	for i := 0; i < len(arr); i++ {
+		arr[i] = i * 2
+	}
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("items at index %d is %d\n", i, arr[i])
+	}
+	var arr2 [3]int
+	f(arr2)
+	fp(&arr2)
 }
