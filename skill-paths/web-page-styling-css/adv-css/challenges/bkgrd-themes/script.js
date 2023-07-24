59e8a257dafbf3fe4ab115699a1e3c0de515cf27
Rebecca Schmelzer <beca.dance@gmail.com>
2023-07-24 14:47:24 -0700

    whatever0

--- skill-paths/web-page-styling-css/adv-css/challenges/bkgrd-themes/script.js
+++ skill-paths/web-page-styling-css/adv-css/challenges/bkgrd-themes/script.js
@@ -1,23 +1,24 @@
-const root = document.documentElement
-const themebtns = document.querySelectorAll('.theme > button')
+const root = document.documentElement 
+const themeBtns = document.querySelectorAll('.theme > button')
 
-themebtns.forEach((btn) => {
-    btn.addEventListener('click', handleThemeUpdate)
+themeBtns.forEach((btn) => {
+  btn.addEventListener('click', handleThemeUpdate)
+  console.log("hello");
 })
 
 function handleThemeUpdate(e) {
-    switch(e.target.value) {
-        case 'dark':
-            root.style.setProperty('--bg', 'black')
-            root.style.setProperty('--bg-text', 'white')
-            break
-        case 'calm':
-            root.style.setProperty('--bg', 'lightblue')
-            root.style.setProperty('--bg-text', 'white')
-            break
-        case 'light':
-            root.style.setProperty('--bg', 'white')
-            root.style.setProperty('--bg-text', 'black')
-            break
-    } 
+  switch(e.target.value) {
+    case 'dark': 
+      root.style.setProperty('--bg', 'black')
+      root.style.setProperty('--bg-text', 'white')
+      break
+    case 'calm': 
+       root.style.setProperty('--bg', '#B3E5FC')
+       root.style.setProperty('--bg-text', '#37474F')
+      break
+    case 'light':
+      root.style.setProperty('--bg', 'white')
+      root.style.setProperty('--bg-text', 'black')
+      break
+  }
 }
