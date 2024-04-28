3005e4e665ddfada516696018f77e602f76b2e47
RebeccaSchmelzer <beca.dance@gmail.com>
2024-04-27 23:31:39 -0400

    some html

--- projects/coffeeshop/main.go
+++ projects/coffeeshop/main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"coffeeshop/coffee"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to the coffee shop!",
-		})
-	})
-	router.GET("/coffee", getCoffee)
-	router.Run(":8080")
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.html")
+
+	r.GET("/home", coffeeList)
+	r.GET("/coffee", getCoffee)
+
+	fmt.Println("Starting the server...")
+	r.Run(":8081")
+}
+
+func coffeeList(c *gin.Context) {
+	coffee, _ := coffee.GetCoffees()
+
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"list": coffee.List,
+		},
+	)
 }
 
 func getCoffee(c *gin.Context) {
