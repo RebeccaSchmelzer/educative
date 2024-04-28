b3c5a28acdffd71089b55faab777f6e52adc380e
RebeccaSchmelzer <beca.dance@gmail.com>
2024-04-27 23:12:37 -0400

    some gin

--- projects/coffeeshop/main.go
+++ projects/coffeeshop/main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"coffeeshop/coffee"
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	coffees, err := coffee.GetCoffees()
-	if err != nil {
-		fmt.Println("Error while getting coffeelist ", err)
-		return
-	}
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to the coffee shop!",
+		})
+	})
+	router.GET("/coffee", getCoffee)
+	router.Run(":8080")
+}
 
-	fmt.Println("Printing the list of coffees available in the CoffeeShop...")
-	for _, element := range coffees.List {
-		result := fmt.Sprintf("%s for $%v", element.Name, element.Price)
-		fmt.Println(result)
-	}
-
-	fmt.Println("Is Latte Available? ", coffee.IsCoffeeAvailable("Latte"))
+func getCoffee(c *gin.Context) {
+	coffeelist, _ := coffee.GetCoffees()
+	c.String(http.StatusOK, " %s", coffeelist)
 }
